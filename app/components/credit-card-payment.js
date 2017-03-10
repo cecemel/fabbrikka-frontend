9d8893d7e96cc13a85319de7938d09b5855f1925
cecemel <ruiz.felixruiz@gmail.com>
2017-03-10 12:41:55 +0100

    first version CC payment (frontend)

--- app/components/credit-card-payment.js
+++ app/components/credit-card-payment.js
@@ -3,29 +3,82 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     scriptDownloaded: false,
     hasRendered: false,
+    card: null,
+    stripeInstance: null,
+    creditCardElement: "credit-card-element",
+    creditCardErrors: "credit-card-errors",
     isPageReadyObserver: Ember.observer('scriptDownloaded', 'hasRendered', function(){
         if(this.get('scriptDownloaded') && this.get('hasRendered')){
             this.mountCart();
         }
     }),
+
     didInsertElement() {
         let self = this;
          Ember.$.getScript('https://js.stripe.com/v3/', () => {
              self.set('scriptDownloaded', true);
          });
      },
+
      didRender(){
          this.set('hasRendered', true);
-        //  if (this.get('isReady')){
-
-        // }
     },
+
     mountCart(){
+        // taken from https://stripe.com/docs/elements
         let stripe = new Stripe('pk_test_Od5jsn7vV50m8Y8OBf6ebmN5');
+        // Create an instance of Elements
         let elements = stripe.elements();
-             // Create an instance of the card Element
-        var card = elements.create('card');
-             // Add an instance of the card Element into the `card-element` <div>
-        card.mount('#card-element');
+
+        // Just inject sytling here
+        var style = {
+          base: {
+            color: '#32325d',
+            lineHeight: '24px',
+            fontFamily: 'Helvetica Neue',
+            fontSmoothing: 'antialiased',
+            fontSize: '16px',
+            '::placeholder': {
+              color: '#aab7c4'
+            }
+          },
+          invalid: {
+            color: '#fa755a',
+            iconColor: '#fa755a'
+          }
+        };
+
+        // Create an instance of the card Element
+        let card = elements.create('card', {style: style});
+
+        // Add an instance of the card Element into the `card-element` <div>
+        card.mount('#' + this.get('creditCardElement'));
+
+        // Handle real-time validation errors from the card Element.
+        card.addEventListener('change', function(event) {
+            const displayError = document.getElementById(this.get('creditCardErrors'));
+            if (event.error) {
+                displayError.textContent = event.error.message;
+            } else {
+                displayError.textContent = '';
+            }
+        });
+
+        this.set('card', card);
+        this.set('stripeInstance', stripe);
+    },
+
+    actions: {
+        submitPayment(){
+            this.get('stripeInstance').createToken(this.get('card'))
+            .then((result) => {
+                if (result.error) {
+                    var errorElement = document.getElementById(this.get('creditCardErrors'));
+                    errorElement.textContent = result.error.message;
+                } else {
+                    console.log('foo');
+                }
+            });
+        }
     }
 });
