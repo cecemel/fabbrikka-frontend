d400f25df851d096d234df9acafcf1a5faa98844
cecemel <ruiz.felixruiz@gmail.com>
2017-03-10 15:10:23 +0100

    added helper

---
+++ tests/unit/helpers/equal-strings-test.js
@@ -0,0 +1,12 @@
+
+import { equalStrings } from 'fabbrikka-frontend/helpers/equal-strings';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | equal strings');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = equalStrings([42]);
+  assert.ok(result);
+});
+
