1d43f0176d1641d48ed5b8240ea05057f6e61e3e
cecemel <ruiz.felixruiz@gmail.com>
2017-03-11 15:01:52 +0100

    testing bancontact

--- app/components/bancontact-payment.js
+++ app/components/bancontact-payment.js
@@ -1,19 +1,33 @@
 import Ember from 'ember';
+import config from 'fabbrikka-frontend/config/environment';
 
 export default Ember.Component.extend({
-    isPageReadyObserver: Ember.observer('scriptDownloaded', 'hasRendered', function(){
-        if(this.get('scriptDownloaded') && this.get('hasRendered')){
-        }
-    }),
-
-    didInsertElement() {
-        let self = this;
-         Ember.$.getScript('https://js.stripe.com/v3/', () => {
-             self.set('scriptDownloaded', true);
-         });
-     },
-
-     didRender(){
-         this.set('hasRendered', true);
+    _initStripe(){
+        return  Ember.$.getScript('https://js.stripe.com/v2/');
     },
+
+    _handleBcnResonse(...args){
+        console.log(args[0]);
+    },
+
+    actions: {
+        startBancontactProcedure(){
+            this._initStripe()
+            .then(() => {
+                console.log('hello');
+                Stripe.setPublishableKey(config.stripe.key);
+                Stripe.source.create({
+                    type: 'bancontact',
+                    amount: 100,
+                    currency: 'eur',
+                    owner: {
+                        name: this.get('accountHolder'),
+                    },
+                    redirect: {
+                        return_url: 'https://google.com',
+                    },
+                }, this._handleBcnResonse);
+            });
+        }
+    }
 });
