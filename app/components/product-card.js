bb24d13057f0401f96778428bb94643186969012
cecemel <ruiz.felixruiz@gmail.com>
2017-03-15 16:38:55 +0100

    changed product card

--- app/components/product-card.js
+++ app/components/product-card.js
@@ -4,10 +4,10 @@ export default Ember.Component.extend({
   primaryImages: Ember.computed.filterBy( 'item.productImages','type', 'primary'),
   primaryImageURL: Ember.computed.oneWay('primaryImages.firstObject.accessURL'),
   name: Ember.computed.oneWay('item.productNames.firstObject.name'),
-  price: Ember.computed( 'item.productPrice', function() {
-    let price = this.get('item.productPrice.amount');
+  price: Ember.computed('item.productVariants.firstObject', function(){
+    let price = this.get('item.productVariants.firstObject.price');
     if( price ) {
       return `${price} €`;
     }
-  } )
+}),
 });
