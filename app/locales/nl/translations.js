6f2daee77e1d040799180509a67d4d4cdeec95dd
cecemel <ruiz.felixruiz@gmail.com>
2017-03-20 11:28:55 +0100

    add translation for bancontact

--- app/locales/nl/translations.js
+++ app/locales/nl/translations.js
@@ -1,8 +1,12 @@
 export default {
     "components.navbar-mat-fabb.shop": "winkel",
     "components.navbar-mat-fabb.about": "over ons",
+
     "components.product-details.plusonesweater": "+1 pulletje, bedankt!",
     "components.product-details.choose-size": "kies een maat...",
     "components.product-details.add-to-cart": "ik wil!",
     "components.product-details.go-to-cart": "naar mijn mandje!",
+
+    "components.bancontact-payment.account-holders-name": "naam op kaart",
+    "components.bancontact-payment.start-payment": "start betaling",
 };
