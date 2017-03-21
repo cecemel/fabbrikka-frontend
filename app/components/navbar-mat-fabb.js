5a63fed1327235452414d38c43ec57aeae849465
cecemel <ruiz.felixruiz@gmail.com>
2017-03-21 15:00:09 +0100

    basic login and hide admin bars

--- app/components/navbar-mat-fabb.js
+++ app/components/navbar-mat-fabb.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+    session: Ember.inject.service(),
     didInsertElement: function() {
         this.$(".dropdown-button").dropdown();
         this.$(".button-collapse").sideNav({closeOnClick: true});
