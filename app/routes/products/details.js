8e95ef1816901e57249ab86d4d8c730e2bda7239
cecemel <ruiz.felixruiz@gmail.com>
2017-03-21 11:21:59 +0100

    smart improvement to make cli swiper work better

--- app/routes/products/details.js
+++ app/routes/products/details.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(params) {
-        return this.store.findRecord('product', params.id);
+        return this.store.findRecord('product', params.id, {include: "product-images" });
     }
 });
