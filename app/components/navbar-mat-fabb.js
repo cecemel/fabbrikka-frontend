e07d5df5b58e5c1f44a6efe55be96e28b398f2d5
cecemel <ruiz.felixruiz@gmail.com>
2017-04-09 11:03:58 +0200

    maide sidenave smaller

--- app/components/navbar-mat-fabb.js
+++ app/components/navbar-mat-fabb.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     session: Ember.inject.service(),
     didInsertElement(){
-        this.$(".button-collapse").sideNav({closeOnClick: true});
+        this.$(".button-collapse").sideNav({closeOnClick: true,  menuWidth: 200});
     },
     didRender(){
         this.$(".dropdown-button").dropdown();
