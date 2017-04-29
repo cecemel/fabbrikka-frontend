fc118b733e0e256f1404a3734fa1cce99814058f
cecemel <ruiz.felixruiz@gmail.com>
2017-04-29 21:39:01 +0200

    forced background reload, not ideal but better than nothing

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        return this.store.findAll('product', {include: "product-names,product-images" });
+        return this.store.findAll('product', {include: "product-names,product-images", reload: true});
     }
 });
