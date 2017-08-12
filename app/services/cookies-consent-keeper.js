d779108bf71ce66d5fa67cf3cbd09a013f8bcbd2
cecemel <ruiz.felixruiz@gmail.com>
2017-08-12 14:41:14 +0200

    template draft of cookies service

---
+++ app/services/cookies-consent-keeper.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+    //by default, the cookies thing should be hidden
+    //then checks if it was present
+    //then shows it
+});
