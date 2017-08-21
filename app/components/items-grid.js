855adc916c754b8cb5a9c00c2a3b5ea6f6863bf2
cecemel <ruiz.felixruiz@gmail.com>
2017-08-21 22:27:46 +0200

    nog een commitje van meg

--- app/components/items-grid.js
+++ app/components/items-grid.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
     _formatRows: function(data){
-        let rows = [], start = 0, stop = 3, size = 3, length = data.get('length');
+        let rows = [], start = 0, stop = 4, size = 4, length = data.get('length');
 
         while(start < length + size - 1){
             rows.push(data.slice(start, stop));
