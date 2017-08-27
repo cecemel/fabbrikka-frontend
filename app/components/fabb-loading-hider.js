3be1830d5d6f6175e8301652c8229cd02fe49c63
cecemel <ruiz.felixruiz@gmail.com>
2017-08-27 13:19:24 +0200

    update

--- app/components/fabb-loading-hider.js
+++ app/components/fabb-loading-hider.js
@@ -4,7 +4,8 @@ export default Ember.Component.extend({
 
   didInsertElement(){
     this._super(...arguments);
-    this.$('.fabb-loading-hider').fadeOut();
-    this.$('.fabb-loading-hider').scrollTop()
+    this.$('.fabb-loading-hider-almost').css('visibility','visible');
+    this.$('.fabb-loading-hider').fadeOut(700);
+    this.$('.fabb-loading-hider').scrollTop();
   }
 });
