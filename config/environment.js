aeab4eb198a88538307ea81838d600881e45b24e
cecemel <ruiz.felixruiz@gmail.com>
2017-09-03 17:06:40 +0200

    setting confi righ

--- config/environment.js
+++ config/environment.js
@@ -61,13 +61,14 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.locationType = 'none';
+    //ENV.locationType = 'none';
 
     // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+    //ENV.APP.LOG_ACTIVE_GENERATION = false;
+    //ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-    ENV.APP.rootElement = '#ember-testing';
+    //ENV.APP.rootElement = '#ember-testing';
+    ENV.APP.backendHost="";
     ENV.APP.publicHostName="https://test.fabbrikka.com";
   }
 
